48362b271d216434fb1b879a2b2a677a78e7c88b
Xiaocheng Dong <xiaocheng.dong@intel.com>
2024-05-15 05:48:05 -0400

    Add crypto and key brocker client
    Signed-off-by: Xiaocheng Dong <xiaocheng.dong@intel.com>

---
+++ crypto.py
@@ -0,0 +1,212 @@
+from abc import ABC, abstractmethod
+import argparse
+import base64
+import logging
+import json
+import os
+import secrets
+import struct
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+
+LOG = logging.getLogger(__name__)
+
+class CryptoBase(ABC):
+    """An abstract base class for crypto.
+    This class serves as a blueprint for subclasses that need to implement
+    `encrypt` and `decrypt` methods for different types of crypto.
+    """
+
+    @abstractmethod
+    def encrypt(self, key, data):
+        """Encrypt data by a key.
+
+        This method is used to encrypt data by a key.
+
+        Args:
+            key (bytes): The key for encryption.
+            data (bytes): The data for encryption.
+
+        Raises:
+            ValueError: If the key or data is None.
+            NotImplementedError: If the subclasses don't implement the method.
+        """
+        raise NotImplementedError("Subclasses should implement encrypt() method.")
+    
+    @abstractmethod
+    def decrypt(self, key, data):
+        """Decrypt data by a key.
+
+        This method is used to decrypt data by a key.
+
+        Args:
+            key (bytes): The key for decryption.
+            data (bytes): The data for decryption.
+
+        Raises:
+            ValueError: If the key or data is None.
+            NotImplementedError: If the subclasses don't implement the method.
+        """
+        raise NotImplementedError("Subclasses should implement decrypt() method.")
+
+class AesCrypto(CryptoBase):
+    """Advanced Encryption Standard (AES) crypto.
+
+    AES ensures data confidentiality by transforming plaintext into ciphertext using a secret key,
+    making it challenging for unauthorized parties to decipher the original data.
+
+    Normally, the encrypted data format can be:
+    -------------------------------------------------------------------
+    | 12 bytes header | [12] bytes IV | encrypted data | [16] bytes tag |
+    -------------------------------------------------------------------
+    and the 12 bytes header:
+    -----------------------------------------------------------
+    | uint32 IV length | uint32 tag length | uint32 data length |
+    -----------------------------------------------------------
+    """
+    def encrypt(self, key, data):
+        """AES encryption.
+
+        Args:
+            data (bytes): The data for encryption.
+            key (bytes): The key for encryption.
+
+        Raises:
+            ValueError: If the data or key is None.
+        """
+        if data is None:
+            raise ValueError("The data can not be None")
+        if key is None:
+            raise ValueError("The key can not be None")
+
+        aesgcm = AESGCM(key)
+        IV = secrets.token_bytes(12)
+        encrypted_data = aesgcm.encrypt(IV, data, None)
+        header = struct.pack('<3I', len(IV), 16, len(encrypted_data))
+        return header + IV + encrypted_data
+
+    def decrypt(self, key, data):
+        """AES encryption.
+
+        Args:
+            data (bytes): The data for encryption.
+            key (bytes): The key for encryption.
+
+        Raises:
+            ValueError: If the data or key is None.
+        """
+        if data is None:
+            raise ValueError("The data can not be None")
+        if key is None:
+            raise ValueError("The key can not be None")
+        
+        header_len = 12
+        iv_len, tag_len, data_len = struct.unpack('<3I', data[:header_len])
+        iv = data[header_len : (iv_len + header_len)]
+        data = data[(iv_len + header_len) : -tag_len]
+        tag = data[-tag_len:]
+
+        LOG.debug("Decrypt data, IV len %d, tag len %d, data len %d", iv_len, tag_len, data_len)
+        decryptor = Cipher(algorithms.AES(key), modes.GCM(iv, tag)).decryptor()
+        decrypted_data = decryptor.update(data) + decryptor.finalize()
+        return decrypted_data
+
+    def encrypt_file(self, key, input, output):
+        """Encrypt a file.
+        Args:
+            key (bytes): The key for encryption.
+            input (str): The input file for encryption.
+            output (str): The output file for encryption.
+
+        Raises:
+            ValueError: If the input or output is None or not a file.
+        """
+        if key is None:
+            raise ValueError("The key can not be None")
+        if input is None:
+            raise ValueError("The input can not be None")
+        if output is None:
+            raise ValueError("The output can not be None")
+        
+        if not os.path.isfile(input):
+            raise ValueError(f"The input is not a file: {input}")
+
+        with open(input, 'rb') as infile:
+            data = infile.read()
+            LOG.info(f"Encrypting file {input}")
+            encrypted_data = self.encrypt(key, data)
+            with open(output, 'wb') as outfile:
+                outfile.write(encrypted_data)
+
+    def decrypt_file(self, key, input, output):
+        """Decrypt a file.
+        Args:
+            key (bytes): The key for decryption.
+            input (str): The input file for decryption.
+            output (str): The output file for decryption.
+
+        Raises:
+            ValueError: If the input or output is None or not a file.
+        """
+        if key is None:
+            raise ValueError("The key can not be None")
+        if input is None:
+            raise ValueError("The input can not be None")
+        if output is None:
+            raise ValueError("The output can not be None")
+        
+        if not os.path.isfile(input):
+            raise ValueError(f"The input is not a file: {input}")
+
+        with open(input, 'rb') as infile:
+            data = infile.read()
+            LOG.info(f"Decrypting file {input}")
+            decrypted_data = self.decrypt(key, data)
+            with open(output, 'wb') as outfile:
+                outfile.write(decrypted_data)
+
+def main():
+    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+    parser = argparse.ArgumentParser(description="The utility to show how to encrypt model")
+    parser.add_argument('-i', '--input',  help='set input file or directory', dest='input')
+    parser.add_argument('-o', '--output',  help='set output file or directory', dest='output')
+    parser.add_argument('-k', '--key',  help='set encryption key with base64 encoded', dest='key')
+    args = parser.parse_args()
+
+    if args.input is None or args.output is None or args.key is None:
+        parser.print_help()
+        exit(1)
+
+    try:
+        key = base64.b64decode(args.key)
+    except Exception as e:
+        LOG.error("Decode the key failed, key should be base64 encoded.")
+        exit(1)
+
+    encryption_config = {
+        "kbs": "",
+        "kbs_url": "",
+        "key_id": "",
+        "files": []
+    }
+
+    crypto = AesCrypto()
+    if os.path.isdir(args.input):
+        if os.path.isdir(args.output):
+            for item in os.listdir(args.input):
+                input_file = os.path.join(args.input, item)
+                if (os.path.isfile(input_file) and
+                    not item.startswith('.') and
+                    not item.endswith('.md')):
+                    output_file = os.path.join(args.output, item + ".aes")
+                    crypto.encrypt_file(key, input_file, output_file)
+                    encryption_config["files"].append(output_file)
+            with open(args.output + "/encryption-config.json", "w") as config_file:
+                json.dump(encryption_config, config_file)
+        else:
+            LOG.error("Output is not matching input.")
+    if os.path.isfile(args.input):
+        crypto.encrypt_file(key, args.input, args.output)
+
+if __name__ == "__main__":
+    main()
---
+++ keybrocker.py
@@ -0,0 +1,173 @@
+"""A Keybroker module.
+
+This module provides an object-oriented design for key broker client to connect with
+key broker server (KBS), get model decryption key from the KBS.
+
+Classes:
+    KeyBrokerClientBase: An abstract base class for key broker client.
+    SimpleKeyBrokerClient: A concrete class implementing the KeyBrokerClientBase to connect
+      a simple KBS.
+"""
+
+from abc import ABC, abstractmethod
+import base64
+import crypto
+import logging
+import struct
+import requests
+
+from ccnp import Eventlog, Quote
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
+LOG = logging.getLogger(__name__)
+
+# Set the connection timeout to 10s
+TIMEOUT = 10
+# Set the connection retry times to 3
+RETRY_TIMES=3
+# Set http connection succesfull code
+HTTP_OK = [200]
+
+class KeyBrokerClientBase(ABC):
+    """An abstract base class for key broker client.
+
+    This class serves as a blueprint for subclasses that need to implement
+    `get_key` methods for different types of KBS.
+    """
+
+    @abstractmethod
+    def get_key(self, server_url: str, key_id: str):
+        """Get a key from KBS.
+
+        This method is used to get a key from KBS.
+
+        Args:
+            server_url (str): The url of KBS.
+            key_id (str): The id of the key.
+
+        Raises:
+            ValueError: If the server_url or key_id is None.
+            RuntimeError: If get quote or get key failed.
+            NotImplementedError: If the subclasses don't implement the method.
+        """
+        raise NotImplementedError("Subclasses should implement get_key() method.")
+
+
+class ItaKeyBrokerClient(KeyBrokerClientBase):
+    """A implementation for key broker client.
+
+    This class implement `get_key` in `KeyBrokerClientBase` abstract base class to connect
+    to a ITA KBS: https://github.com/intel/trustauthority-kbs
+    Here is an example flow of a ITA KBS:
+        - Accept a quote and a public key from client.
+        - Verify the quote and do attestation, return if verify failed.
+        - Get the user key from key management server (KMS), generate a symmetric wrapping
+          key (SWK) to encrypt the user key (wrapped_key).
+        - Encrypt the SWK by the public key from client (wrapped_swk).
+    For a key broker client, here is an example flow to get a key from KBS:
+        - Get and replay all event logs, and verify by the measurement register.
+        - Generate 2048 bit RSA key pair (a public key and a private key).
+        - Encode the public key to base64 for transferring (user_data).
+        - Get quote in the TEE with the hash of the public key for measurement (quote).
+        - Request wrapped_key and wrapped_swk from KBS with quote and user_data.
+        - Decrypt the user key by the SWK.
+    """
+    def get_key(self, server_url: str, key_id: str) -> bytes: # pylint: disable=too-many-locals
+        """Get model key by key ID from the KBS.
+  
+        This method get and replay all event logs, and verify by the measurement register, then
+        construct the request headers and body to request the wrapped_key and wrapped_swk from KBS,
+        decrypt the user key by SWK and return the key.
+
+        A example requests and response:
+            - request headers:
+                Accept:application/json
+                Content-Type:application/json
+                Attestation-Type:TDX
+            - request body:
+                {
+                    "quote":"",
+                    "user_data":""
+                }
+            - response body:
+                {
+                    "wrapped_key":"",
+                    "wrapped_swk":""
+                }
+
+        Args:
+            server_url (str): The url of KBS.
+            key_id (str): The id of the key.
+
+        Returns:
+            bytes: The bytes of the key.
+    
+        Raises:
+            ValueError: If the server_url or key_id is None.
+            RuntimeError: If get or verify event log failed, and if get quote or get key failed.
+            NotImplementedError: If the subclasses don't implement the method.
+        """
+        if server_url is None:
+            raise ValueError("KBS server url can not be None")
+        if key_id is None:
+            raise ValueError("KBS key id can not be None")
+
+        private_key = rsa.generate_private_key(public_exponent=65537, key_size=3072)
+        pubkey = private_key.public_key()
+        pubkey_der = pubkey.public_bytes(encoding=serialization.Encoding.DER,
+                                             format=serialization.PublicFormat.SubjectPublicKeyInfo)
+
+        LOG.debug("Getting TDX Quote by CCNP")
+        user_data = base64.b64encode(pubkey_der).decode('utf-8')
+        quote = Quote.get_quote(user_data=user_data)
+        if quote is None:
+            raise RuntimeError("Get TDX Quote failed")
+        quote = base64.b64encode(quote.quote).decode('utf-8')
+
+        req_body = {
+            "quote": quote,
+            "user_data": user_data
+        }
+
+        headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Attestation-Type": "TDX"
+        }
+
+        LOG.debug("Getting key from the KBS")
+        resp = None
+        for _ in range(RETRY_TIMES):
+            try:
+                resp = requests.post(server_url, json=req_body, headers=headers, verify=False,
+                                     timeout=TIMEOUT)
+                if resp.status_code in HTTP_OK:
+                    break
+            except requests.exceptions.ConnectionError:
+                LOG.debug("Connect error from the KBS, try again")
+
+        if resp is None or resp.status_code not in HTTP_OK:
+            raise RuntimeError("Unexpected response from the KBS")
+
+        resp_body = resp.json()
+        if "wrapped_key" not in resp_body or "wrapped_swk" not in resp_body:
+            raise RuntimeError("Empty key response from the KBS")
+
+        wrapped_key = base64.b64decode(resp_body['wrapped_key'])
+        wrapped_swk = base64.b64decode(resp_body['wrapped_swk'])
+
+        LOG.debug("Decrypting the SWK")
+        swk = private_key.decrypt(
+          wrapped_swk,
+          padding.OAEP(
+              mgf=padding.MGF1(algorithm=hashes.SHA256()),
+              algorithm=hashes.SHA256(),
+              label=None
+          )
+        )
+        crypt = crypto.AesCrypto()
+        return crypt.decrypt(wrapped_key, swk)
